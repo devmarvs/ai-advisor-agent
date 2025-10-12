eddf0b76620db982ec283cad6e3d13fe6e25f3c7
devmarvs <marvin.dev.ph@gmail.com>
2025-10-12 07:11:25 +0200

    fixed syntax error

--- api/handlers/cron.go
+++ api/handlers/cron.go
@@ -1,10 +1,22 @@
 package handlers
 
 import (
-  "database/sql"
-  "net/http"
-  "os"
-  "github.com/gin-gonic/gin"
+	"database/sql"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
-func CronTick(db *sql.DB) gin.HandlerFunc { return func(c *gin.Context){ if c.GetHeader("Authorization")!="Bearer "+os.Getenv("CRON_TOKEN"){ c.Status(http.StatusUnauthorized); return } c.JSON(http.StatusOK, gin.H{"ok":true}) } }
+func CronTick(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Simple token check to protect the endpoint
+		if c.GetHeader("Authorization") != "Bearer "+os.Getenv("CRON_TOKEN") {
+			c.Status(http.StatusUnauthorized)
+			return
+		}
+
+		// TODO: enqueue polling tasks, etc.
+		c.JSON(http.StatusOK, gin.H{"ok": true})
+	}
+}
